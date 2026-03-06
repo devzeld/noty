bb507145411328681c1a9d2ecb58da7969a9f3ca
devzeld <zeld.exe.dev@gmail.com>
2026-03-06 11:48:49 +0100

    minor fix

--- backend/src/notes.php
+++ backend/src/notes.php
@@ -7,21 +7,17 @@ $db = DBHandler::getPDO();
 $method = $_SERVER["REQUEST_METHOD"];
 
 switch ($method) {
-    // GET: fetch all notes or just one note by its id or assigned tags
     case "GET":
-        echo json_encode(["data" => "get"]);
+        echo json_encode(["data" => 'get']);
         break;
-    // POST: create a note
     case "POST":
         $data = json_decode(file_get_contents("php://input"));
         echo json_encode(["data" => "post"]);
         break;
-    // PUT: update a note
     case "PUT":
         $data = json_decode(file_get_contents("php://input"));
         echo json_encode(["data" => "put"]);
         break;
-    // DELETE: set a note to deleted
     case "DELETE":
         $data = json_decode(file_get_contents("php://input"));
         echo json_encode(["data" => "delete"]);
---
+++ backend/src/tags.php
@@ -0,0 +1,28 @@
+<?php
+require "../config/connect.php";
+require "../config/cors.php";
+
+$db = DBHandler::getPDO();
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+switch ($method) {
+    case "GET":
+        echo json_encode(["data" => 'get']);
+        break;
+    case "POST":
+        $data = json_decode(file_get_contents("php://input"));
+        echo json_encode(["data" => "post"]);
+        break;
+    case "PUT":
+        $data = json_decode(file_get_contents("php://input"));
+        echo json_encode(["data" => "put"]);
+        break;
+    case "DELETE":
+        $data = json_decode(file_get_contents("php://input"));
+        echo json_encode(["data" => "delete"]);
+        break;
+    default:
+        echo json_encode(["message" => "Unsupported request method"]);
+        break;
+}
